3a0d661428079a79a4cee4a99b8f75ab83d1736b
Laulou <1053374792@qq.com>
2023-07-01 00:16:38 +0800

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   parserOptions: {
     ecmaVersion: 2023, // Allows for the parsing of mode`rn ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
   },
   plugins: ['vue'],
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/eslint-config-prettier',
     'eslint:recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   rules: {
     'vue/array-bracket-spacing': ['error', 'never'],
@@ -21,19 +21,20 @@ module.exports = {
     'prettier/prettier': [
       2,
       {
+        wrapAttributes: false,
         bracketSpacing: false,
         printWidth: 120,
         semi: false,
         singleQuote: true,
         tabWidth: 2,
-        trailingComma: 'es5'
-      }
+        trailingComma: 'es5',
+        proseWrap: 'preserve',
+      },
     ],
     'handle-callback-err': 0,
     'prefer-promise-reject-errors': [2, { allowEmptyReject: true }],
     'no-prototype-builtins': 0,
-    'comma-dangle': 0,
     camelcase: 0,
-    eqeqeq: 0
-  }
+    eqeqeq: 0,
+  },
 }
